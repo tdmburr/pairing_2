20e0013fd34f52232513a002761c62511fe1cd1a
Trey Marshall <marshall.trey92@yahoo.com>
2022-11-09 17:59:23 -0600

    Add good friend message

--- friends.js
+++ friends.js
@@ -1,8 +1,11 @@
-var friend = "Liam"
+var friend = "John"
 var bestFriend = "Liam"
+var goodFriend = "Susan"
 
 if (friend === bestFriend) {
     console.log("Let's get pizza")
+} else if (friend === goodFriend) {
+    console.log("Let's get icecream")
 } else {
     console.log("Maybe tomorrow")
 }
